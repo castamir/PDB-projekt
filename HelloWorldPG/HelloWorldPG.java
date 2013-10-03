802681cd6888a698228abf4240974c063a5333c6
Pavel Gajdoš <pavel.gajdos@gmail.com>
2013-10-03 20:26:41 +0200

    issue #2

--- HelloWorldPG/HelloWorldPG.java
+++ HelloWorldPG/HelloWorldPG.java
@@ -23,7 +23,7 @@ public class HelloWorldPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Pavel says 'Hello World!' Hello to you!"); // Display the string. NOVA CAST KOMENTARE. DALSI CAST
+        System.out.println("Pavel says 'Hello World!' Hello to you! ISSUE"); // Display the string. NOVA CAST KOMENTARE. DALSI CAST
     }
     
 }
