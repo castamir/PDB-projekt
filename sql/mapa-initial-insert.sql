81fdb43026d7b44afe8a7dd4fdbad20e16cebfd7
mafian89 <mafian89@gmail.com>
2013-12-06 14:33:50 +0100

    mapka zmena souradneho systemu

--- sql/mapa-initial-insert.sql
+++ sql/mapa-initial-insert.sql
@@ -5,8 +5,8 @@ DELETE FROM mapa;
 INSERT INTO mapa VALUES (
 	'Hotel',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
-		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(50,500, 350,950)
+		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-up, right-bottom)
+		SDO_ORDINATE_ARRAY(20,20, 140,200)
 	)
 );
 
@@ -14,7 +14,7 @@ INSERT INTO mapa VALUES (
 	'Služby u bazénu',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 1), -- exterior polygon (counterclockwise)
-		SDO_ORDINATE_ARRAY(50,50, 350,50, 350,450, 50,450, 50,400, 250,400, 250,100, 50,100, 50,50)
+		SDO_ORDINATE_ARRAY(20,220, 140,220, 140,380, 20,380, 20,360, 100,360, 100,240, 20,240, 20,220)
 	)
 );
 
@@ -22,7 +22,7 @@ INSERT INTO mapa VALUES (
 	'Bazén',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(50,200, 200,350)
+		SDO_ORDINATE_ARRAY(20,260, 80,340)
 	)
 );
 
@@ -30,7 +30,7 @@ INSERT INTO mapa VALUES (
 	'Bar+Disko',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(400,800, 700,950)
+		SDO_ORDINATE_ARRAY(160,20, 280,80)
 	)
 );
 
@@ -38,7 +38,7 @@ INSERT INTO mapa VALUES (
 	'Tenisové kurty',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(400,500, 700,750)
+		SDO_ORDINATE_ARRAY(160,100, 280,200)
 	)
 );
 
@@ -47,7 +47,7 @@ INSERT INTO mapa VALUES (
 	'Wellness',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(800,550, 950,950)
+		SDO_ORDINATE_ARRAY(320,20, 380,180)
 	)
 );
 
@@ -55,7 +55,7 @@ INSERT INTO mapa VALUES (
 	'Hlídání dětí',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 3), -- exterior rectangle (left-bottom, right-top)
-		SDO_ORDINATE_ARRAY(800,400, 950,500)
+		SDO_ORDINATE_ARRAY(320,200, 380,240)
 	)
 );
 
@@ -63,7 +63,7 @@ INSERT INTO mapa VALUES (
 	'Golfové hřiště',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
 		SDO_ELEM_INFO_ARRAY(1, 1003, 1), -- exterior polygon (counterclockwise)
-		SDO_ORDINATE_ARRAY(500,50, 950,50, 950,350, 650,350, 650,250, 500,250, 500,50)
+		SDO_ORDINATE_ARRAY(280,260, 380,260, 380,380, 200,380, 200,300, 280,300, 280,260)
 	)
 );
 
