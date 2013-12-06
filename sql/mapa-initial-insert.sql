17c5204dc8de20d5a31d629633bf3c6a1803c687
mafian89 <mafian89@gmail.com>
2013-12-06 17:54:21 +0100

    mapka 2 + 1 okres

--- sql/mapa-initial-insert.sql
+++ sql/mapa-initial-insert.sql
@@ -1,7 +1,7 @@
 -- poloha bodu [0,0] je zvolena do leveho dolniho rohu mapy
 
 DELETE FROM mapa;
-
+/*
 INSERT INTO mapa VALUES (
 	'Hotel',
 	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
@@ -66,7 +66,21 @@ INSERT INTO mapa VALUES (
 		SDO_ORDINATE_ARRAY(280,260, 380,260, 380,380, 200,380, 200,300, 280,300, 280,260)
 	)
 );
-
+*/
+/*      od 8 bodu - 
+        162,100, 187,100, 200,75,
+        237,75, 237,62, 287,62, 300,50, 325,50, 350,37, 362,37, 350,25, 350,0, 362,0, 400,12, 400,37,
+        412,37, 412,50,*/
+INSERT INTO mapa VALUES (
+	'Karlovarsky',
+	SDO_GEOMETRY(2003, NULL, NULL, -- 2D polygon
+		SDO_ELEM_INFO_ARRAY(1, 1003, 1), -- exterior polygon (clockwise)
+		SDO_ORDINATE_ARRAY(
+        12,125, 25,112, 37,112, 50,138, 87,112, 112,112, 137,100, 150,112, 175,137, 187,137, 187,200,
+        175,225, 162,212, 150,212, 125,237, 112,225, 100,225, 87,237, 50,200, 25,162, 25,150, 12,125 
+        )
+	)
+);
 
 COMMIT;
 
