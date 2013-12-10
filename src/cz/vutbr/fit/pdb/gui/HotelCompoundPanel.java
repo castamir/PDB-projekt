07004c6fbf7d7dd28e72b33319ceb7de8579a19a
castamir <miras.paulik@email.cz>
2013-12-11 00:56:11 +0100

    typos, reformated, improved building type detecting - label in Sluzby now shows chosen building

--- src/cz/vutbr/fit/pdb/gui/HotelCompoundPanel.java
+++ src/cz/vutbr/fit/pdb/gui/HotelCompoundPanel.java
@@ -2,6 +2,7 @@ package cz.vutbr.fit.pdb.gui;
 
 import cz.vutbr.fit.pdb.application.ServiceLocator;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.*;
@@ -27,8 +28,8 @@ public class HotelCompoundPanel extends JPanel implements MouseListener {
 
     private static final long serialVersionUID = 1L;
     //private double zoom = 1.0;
-    
     private List<Shape> shapes;
+    private Sluzby parentPanel;
 
     public HotelCompoundPanel() {
         this.addMouseListener(this);
@@ -71,12 +72,12 @@ public class HotelCompoundPanel extends JPanel implements MouseListener {
         //g.clearRect(0, 0, getWidth(), getHeight());
         super.paint(g);
         /*AffineTransform a = new AffineTransform();
-        AffineTransform a2 = new AffineTransform();
-        a2.setToIdentity();
-        a2.translate(getParent().getWidth() - this.getWidth(), getParent().getHeight() - this.getHeight());
-        a.concatenate(a2);
-        a.scale(zoom, zoom);*/
-        
+         AffineTransform a2 = new AffineTransform();
+         a2.setToIdentity();
+         a2.translate(getParent().getWidth() - this.getWidth(), getParent().getHeight() - this.getHeight());
+         a.concatenate(a2);
+         a.scale(zoom, zoom);*/
+
         Graphics2D g2D = (Graphics2D) g;
         //super.paint(g2D);
 
@@ -107,9 +108,14 @@ public class HotelCompoundPanel extends JPanel implements MouseListener {
         try (Connection conn = ods.getConnection(); Statement stmt = conn.createStatement(); ResultSet resultSet = stmt.executeQuery("select nazev from areal where SDO_RELATE(geometrie, SDO_GEOMETRY(2001, NULL, SDO_POINT_TYPE(" + x + "," + y + ",NULL), NULL, NULL), 'mask=contains') = 'TRUE'")) {
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("nazev"));
+                parentPanel.updateTitle(resultSet.getString("nazev"));
             }
         }
 
+    }
+
+    public void setParentPanel(Sluzby panel) {
+        parentPanel = panel;
     }
 
 
@@ -141,10 +147,9 @@ public class HotelCompoundPanel extends JPanel implements MouseListener {
     @Override
     public void mouseExited(MouseEvent e) {
     }
-    
     /*
-    public void setZoom(double zoomFactor) {
-        zoom = zoomFactor;
-        repaint();
-    }*/
+     public void setZoom(double zoomFactor) {
+     zoom = zoomFactor;
+     repaint();
+     }*/
 }
