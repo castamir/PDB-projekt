58478a01710ce66428da3fdce7f558f3751ffde8
Pavel Gajdoš <pavel.gajdos@gmail.com>
2013-12-15 14:37:48 +0100

    metoda pro ziskani sezmanu pokoju

--- src/cz/vutbr/fit/pdb/models/RezervaceModel.java
+++ src/cz/vutbr/fit/pdb/models/RezervaceModel.java
@@ -6,10 +6,13 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import oracle.jdbc.pool.OracleDataSource;
 
 /**
@@ -68,6 +71,25 @@ public class RezervaceModel extends BaseModel {
                 while (rs.next()) {
                     pokoje.add(rs.getInt("pokoj"));
                 }
+            }
+        }
+        
+        return pokoje;
+    }
+    
+    
+    public Map<Integer, String> getPokoje() throws SQLException {
+        
+        Map<Integer, String> pokoje = new LinkedHashMap<>();
+        
+        OracleDataSource ods = ServiceLocator.getConnection();
+        try (Connection conn = ods.getConnection(); 
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM pokoje")
+             )
+        {
+            while (rs.next()) {
+                pokoje.put(rs.getInt("id"), rs.getString("nazev"));
             }
         }
         
