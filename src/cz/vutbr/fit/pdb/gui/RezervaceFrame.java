f2c81222fc036bcd10ae4d0014fae50d85a90cea
castamir <miras.paulik@email.cz>
2013-12-17 01:08:09 +0100

    aktualizace prehledu po zavreni okna nove rezervace

---
+++ src/cz/vutbr/fit/pdb/gui/RezervaceFrame.java
@@ -0,0 +1,28 @@
+package cz.vutbr.fit.pdb.gui;
+
+import java.awt.HeadlessException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Paulík Miroslav
+ * @author Mikulica Tomáš
+ * @author Gajdoš Pavel
+ */
+public class RezervaceFrame extends JFrame {
+
+    private PrehledRezervaci pz;
+
+    public RezervaceFrame(PrehledRezervaci prehledRezervaci) throws HeadlessException {
+        pz = prehledRezervaci;
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("zaviram okno");
+                pz.updateTable();
+            }
+        });
+    }
+}
